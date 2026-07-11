9d8625d24c13afe1d1c0f89759f5b5cd3c354f43
Peeyush <piyushsaini766@gmail.com>
2026-07-11 08:11:19 +0530

    Fix CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,9 +7,13 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://ai-interview-prep-platform-six.vercel.app",
+        "https://ai-interview-prep-platform-cpxmnidod-aim1719.vercel.app"
+    ],
     credentials: true
-}))
+}));
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
